70a5315c0346cb75220b290c77896918c57b68b3
bandanamainali <75675904+bandanamainali@users.noreply.github.com>
2021-03-28 12:39:22 +0545

    Add files via upload

---
+++ lab-6/Employee.php
@@ -0,0 +1,13 @@
+<?php 
+class Employee
+{
+	public $name;
+	public $address;
+
+	public function __construct($name, $address)
+	{
+		$this->name = $name;
+		$this->address = $address;
+	}
+}
+?>
---
+++ lab-6/qno4.php
@@ -0,0 +1,6 @@
+<?php
+$name = "bandana";
+ echo strlen($name); //for counting length
+ echo "   ";
+echo strtoupper($name);//changing string in upper case
+?>
