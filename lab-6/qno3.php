3eb5c2ebebda763cd1db7f7328cd8b4dcfc9c912
bandanamainali <75675904+bandanamainali@users.noreply.github.com>
2021-03-28 12:41:35 +0545

    lab6 added

---
+++ lab-6/qno3.php
@@ -0,0 +1,9 @@
+<?php
+
+$age = array("bandana"=>"19", "anita"=>"25", "zeneya"=>"8");
+
+foreach($age as $x => $x_value) {
+  echo "Key=" . $x . ", Value=" . $x_value;
+  echo "<br>";
+}
+?>
---
+++ lab-6/qno5.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$name = array("bandana", "anita", "zeneya");
+sort($name);
+
+$clength = count($name);
+for($x = 0; $x < $clength; $x++) {
+  echo $name[$x];
+  echo "<br>";
+}
+?>
+<?php
+$name = array("bandana", "anita", "zeneya");
+rsort($name);
+
+$clength = count($name);
+for($x = 0; $x < $clength; $x++) {
+  echo $name[$x];
+  echo "<br>";
+}
+?>
+<?php
+$age = array("bandana"=>"35", "anita"=>"40", "zeneya"=>"20");
+asort($age);
+
+foreach($age as $x => $x_value) {
+  echo "Key=" . $x . ", Value=" . $x_value;
+  echo "<br>";
+}
+?>
+<?php
+$age = array("bandana"=>"35", "anita"=>"40", "zeneya"=>"20");
+ksort($age);
+
+foreach($age as $x => $x_value) {
+  echo "Key=" . $x . ", Value=" . $x_value;
+  echo "<br>";
+}
+?>
+<?php
+$age = array("bandana"=>"35", "anita"=>"40", "zeneya"=>"20");
+arsort($age);
+
+foreach($age as $x => $x_value) {
+  echo "Key=" . $x . ", Value=" . $x_value;
+  echo "<br>";
+}
+?>
+<?php
+$age = array("bandana"=>"35", "anita"=>"40", "zeneya"=>"20");
+krsort($age);
+
+foreach($age as $x => $x_value) {
+  echo "Key=" . $x . ", Value=" . $x_value;
+  echo "<br>";
+}
+?>
+</body>
+</html>
+
