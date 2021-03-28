0a10755a4e9e7f2974e0a5dc60be4b6ff5e8ff38
bandanamainali <75675904+bandanamainali@users.noreply.github.com>
2021-03-28 12:49:26 +0545

    lab6 added

--- lab-6/qno4.php
+++ lab-6/qno4.php
@@ -3,4 +3,28 @@ $name = "bandana";
  echo strlen($name); //for counting length
  echo "   ";
 echo strtoupper($name);//changing string in upper case
+
+$numbers = [10, 15, 12, 20, 6, 2];
+echo "Array<br>";
+foreach ($numbers as $number) {
+	echo "$number ";
+}
+echo "<br><br>Array Functions<br><br>";
+echo "Length of array<br>";
+echo count($numbers);
+echo "<br>Array Push 100<br>";
+array_push($numbers, 100);
+foreach ($numbers as $number) {
+	echo $number."<br>";
+}
+echo "<br>Array POP 100<br>";
+array_pop($numbers);
+foreach ($numbers as $number) {
+	echo $number."<br>";
+}
+echo "<br>Array Sorted in Ascending Order<br>";
+sort($numbers);
+foreach ($numbers as $number) {
+	echo $number."<br>";
+}
 ?>
